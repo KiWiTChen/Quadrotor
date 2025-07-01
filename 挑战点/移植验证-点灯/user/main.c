fc9619bb19581c59cd4a954acd06e9dc13999850
KiWiTChen <3409130145@qq.com>
2025-07-01 13:24:08 +0800

    partly fixed

--- 挑战点/移植验证-点灯/user/main.c
+++ 挑战点/移植验证-点灯/user/main.c
@@ -7,6 +7,8 @@
 OS_STK  TASK1stk[APP_CFG_STARTUP_TASK_STK_SIZE];
 OS_STK  TASK2stk[APP_CFG_STARTUP_TASK_STK_SIZE];
 
+INT8U *pname;
+INT8U err;
 
 int main(void){
 
@@ -19,9 +21,19 @@ int main(void){
 	
 	OSTaskCreate(TASK1,(void*)0,(OS_STK*)&TASK1stk[APP_CFG_STARTUP_TASK_STK_SIZE-1], 5);
 	OSTaskCreate(TASK2,(void*)0,(OS_STK*)&TASK2stk[APP_CFG_STARTUP_TASK_STK_SIZE-1], 6);
+	
+	
+	// 设置任务名
+	OSTaskNameSet(5, (INT8U *)"LED_ON", &err);
+	OSTaskNameSet(6, (INT8U *)"LED_OFF", &err);
+	
+	//获得任务名并将其传给Systemview
+	OSTaskNameGet(5, &pname, &err);
+	SEGGER_SYSVIEW_NameResource((U32)OSTCBPrioTbl[5], (const char *)pname);
+	OSTaskNameGet(6, &pname, &err);
+	SEGGER_SYSVIEW_NameResource((U32)OSTCBPrioTbl[6], (const char *)pname);
+	
 	OSStart();
-	
-	
-	return(0);
+	//return(0);
 	
 }
